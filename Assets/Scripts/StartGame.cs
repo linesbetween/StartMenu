5de4046cd0ee213f483beee4d7a7675d4fd3fb77
linesbetween <14285266.linesbetween@users.noreply.github.com>
2018-10-14 03:39:32 -0400

    inital, jump not working

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+
+	public void LoadScene(int level){
+		Application.LoadLevel (level);
+	}
+	// Use this for initialization
+	void Start () {
+		print ("press");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
