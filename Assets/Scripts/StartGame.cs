a52f1f07a6a9f879f0ccb190bc6960973db0faea
linesbetween <14285266.linesbetween@users.noreply.github.com>
2018-10-14 04:21:10 -0400

    working

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class StartGame : MonoBehaviour {
 
 
 	public void LoadScene(int level){
-		Application.LoadLevel (level);
+		//Application.LoadLevel (level);
+		SceneManager.LoadScene(level);
+		print ("pressed");
 	}
 	// Use this for initialization
 	void Start () {
-		print ("press");
+		print ("press start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//print ("press update");
 	}
 }
+
+
